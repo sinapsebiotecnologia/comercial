1defbd153d55ad181df9a429c7fb82e288cfb4d0
KndHvH <delend89@gmail.com>
2022-05-11 01:36:48 -0300

    home

--- similar/writetxt.py
+++ similar/writetxt.py
@@ -3,14 +3,10 @@ import pandas
 
 
 def main():
-    x = st.file_uploader('arquivo')
 
-    st.text(x)
-
-    if x != 'None':
-        excel_data_df = pandas.read_excel('teste.xlsx')
-        json_str = excel_data_df.to_json()
-        st.text(json_str)
+    excel_data_df = pandas.read_excel('teste.xlsx')
+    json_str = excel_data_df.to_json()
+    st.text(json_str)
 
 if __name__ == '__main__':
     main()
