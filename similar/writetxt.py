69ebffcac8bb345954aa00b34472b53acb750717
KndHvH <delend89@gmail.com>
2022-05-11 01:24:52 -0300

    home

--- similar/writetxt.py
+++ similar/writetxt.py
@@ -7,10 +7,10 @@ def main():
 
     st.text(x)
 
-    # if x != 'None':
-    #     excel_data_df = pandas.read_excel(x)
-    #     json_str = excel_data_df.to_json()
-    #     st.text(json_str)
+    if x != 'None':
+        excel_data_df = pandas.read_excel('teste.xlsx')
+        json_str = excel_data_df.to_json()
+        st.text(json_str)
 
 if __name__ == '__main__':
     main()
