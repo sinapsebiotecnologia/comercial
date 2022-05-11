71a5622636fd41b8faf260ff5f5c1e7c3bb46115
mathe <kk>
2022-05-10 21:41:05 -0300

    fiap

--- similar/main.py
+++ similar/main.py
@@ -1,5 +1,22 @@
-import requests as req
-
-dict = req.get('teste.json')
+import json
+dict = json.load(open('teste.json'))
 
 print(dict)
+print(len(dict))
+
+op = input("produto x ou y?: ")
+
+produto = input("codigo produto: ")
+
+if op == "x":
+    for i in range(len(dict)):
+        if dict[i]["x"] == produto:
+            resultado = dict[i]["y"]
+            break
+else:
+    for i in range(len(dict)):
+        if dict[i]["y"] == produto:
+            resultado = dict[i]["x"]
+            break
+
+print(f"O produto {produto}, e similar ao produto {resultado}")
