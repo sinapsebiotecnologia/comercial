2a4bc7e87a31214872ee8e9fa6d40d7470d99a0c
mathe <kk>
2022-05-10 22:37:54 -0300

    fiap

--- similar/main.py
+++ similar/main.py
@@ -4,19 +4,19 @@ dict = json.load(open('teste.json'))
 print(dict)
 print(len(dict))
 
-op = input("produto x ou y?: ")
+op = input("produto ORIGINAL ou SIMILAR?: ")
 
 produto = input("codigo produto: ")
 
-if op == "x":
+if op == "ORIGINAL":
     for i in range(len(dict)):
-        if dict[i]["x"] == produto:
-            resultado = dict[i]["y"]
+        if dict[i]["ORIGINAL"] == produto:
+            resultado = dict[i]["SIMILAR"]
             break
 else:
     for i in range(len(dict)):
-        if dict[i]["y"] == produto:
-            resultado = dict[i]["x"]
+        if dict[i]["SIMILAR"] == produto:
+            resultado = dict[i]["ORIGINAL"]
             break
 
 print(f"O produto {produto}, e similar ao produto {resultado}")
