f43a7941c57cf94d33039db17e8392bf403a456b
mathe <kk>
2022-05-11 13:29:48 -0300

    fiap

--- similar/main.py
+++ similar/main.py
@@ -1,37 +1,41 @@
 import json
 import pandas
+import requests as req
 import streamlit as st
 
 def main():
+    if st.checkbox("Comparar"):
+        #1
+        dict = req.get("https://sinapsebiotecnologia.github.io/teste.json")
+        dict = dict.json()
 
-    #1
-    dict = json.load(open('teste.json'))
+        #2
+        op = st.radio('Deseja pesquisar por qual tipo?', ("Sinapse","Concorrente"), index=0)
 
-    #2
-    op = input("produto ORIGINAL ou SIMILAR?: ")
-    produto = input("codigo produto: ")
+        produto = st.text_input("codigo produto: ")
 
-    #3
-    resultado = 'None'
-    if op == "ORIGINAL":
+        #3
+        resultado = 'None'
+        if op == "Sinapse":
+            for i in range(len(dict)):
+                if dict[i]["Sinapse"] == produto:
+                    resultado = dict[i]["Concorrente"]
+                    break
         for i in range(len(dict)):
-            if dict[i]["ORIGINAL"] == produto:
-                resultado = dict[i]["SIMILAR"]
+            if dict[i]["Concorrente"] == produto:
+                resultado = dict[i]["Sinapse"]
                 break
-    for i in range(len(dict)):
-        if dict[i]["SIMILAR"] == produto:
-            resultado = dict[i]["ORIGINAL"]
-            break
 
-    #4
-    if resultado == 'None':
-        print(f"Correspondente nao encontrado! (se achar que isso é um erro, favor verificar arquivo excel)")
-    else:
-        print(f"O produto {produto}, e similar ao produto {resultado}")
+        #4
+        if st.button("Verificar"):
+            if resultado == 'None':
+                st.error(f"Correspondente nao encontrado! (se achar que isso é um erro, favor verificar arquivo excel)")
+            else:
+                st.sucess(f"O produto {produto}, e similar ao produto {resultado}")
 
-    #5
-    excel_data_df = pandas.read_excel('teste.xlsx')
-    json_str = excel_data_df.to_json()
+        #5
+        # excel_data_df = pandas.read_excel('teste.xlsx')
+        # json_str = excel_data_df.to_json()
 
 
 
