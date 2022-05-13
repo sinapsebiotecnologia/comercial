a1746a43b4fc7161f2a407ee68c0be461bdbb407
mathe <kk>
2022-05-13 10:42:34 -0300

    fiap

--- s.py
+++ s.py
@@ -78,14 +78,13 @@ def main():
         if st.button("Verificar"):
             if operacao == 10:
                 st.info(f'TES: {tes}')
-                if tes == 954:
-                    st.warning("**A remessa das mercadorias, deverá conter destaque do ICMS devido, " \
-                        "contendo além das informações previstas na legislação.**" \
-                        "\n- Destinatário, aquele determinado pelo adquirente (Cliente);" \
-                        "\n- Natureza da operação, a expressão \"Remessa por conta e ordem de terceiros\";" \
-                        "\n- \"Chave de Acesso da NF-e Referenciada\", a chave de acesso da NF-e relativa ao faturamento (NFE 1);" \
-                        "\n- \"Informações Complementares\", a expressão \"NF-e emitida nos termos do artigo 129-A do RICMS/2000-SP\" e " \
-                        "\" Ajuste Sinief 13/2013\".")
+                st.warning("**A remessa das mercadorias, deverá conter destaque do ICMS devido, " \
+                    "contendo além das informações previstas na legislação.**" \
+                    "\n- Destinatário, aquele determinado pelo adquirente (Cliente);" \
+                    "\n- Natureza da operação, a expressão \"Remessa por conta e ordem de terceiros\";" \
+                    "\n- \"Chave de Acesso da NF-e Referenciada\", a chave de acesso da NF-e relativa ao faturamento (NFE 1);" \
+                    "\n- \"Informações Complementares\", a expressão \"NF-e emitida nos termos do artigo 129-A do RICMS/2000-SP\" e " \
+                    "\" Ajuste Sinief 13/2013\".")
             else:
                 b = f'TES: {tes}'
                 st.info(b)
